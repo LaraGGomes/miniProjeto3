937e19f9242124c281d34d7d896bcc73a27cce2d
Ana Carolina de Sousa Camilo <anacarolinacamilo31@gmail.com>
2025-05-08 23:44:52 -0300

    implementação da criação de usuários

--- fastapi/api/deps.py
+++ fastapi/api/deps.py
@@ -1,15 +1,26 @@
 from typing import Annotated
+from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from jose import jwt, JWTError
 from dotenv import load_dotenv
 import os 
+from .database import SessionLocal
 
 load_dotenv()
 
 SECRET_KEY = os.getenv('AUTH_SECRET_KEY')
 ALGORITHM = os.getenv('AUTH_ALGORITHM')
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth\token')
--- fastapi/api/model.py
+++ fastapi/api/model.py
@@ -1,14 +1,29 @@
-from sqlmodel import Field, SQLModel
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, UniqueConstraint, Table
+from sqlalchemy.orm import relationship
+from .database import Base
 
-class users(SQLModel, table= True):
-    users_id: str = Field(default=None, primary_key=True)
-    senha:    str
+# Modelos SQLAlchemy
+User_Post_association = Table(
+    'user_post', Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('post_id', Integer, ForeignKey('posts.id')),
+)
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    posts = relationship('Post', secondary=User_Post_association, back_populates='users')
+    
 
-class posts(SQLModel, table= True):
-    post_id:  int = Field(default=None, primary_key=True)
-    post:     str
-    users_id: str = Field(foreign_key="users.users_id")
-
-class interaction(SQLModel, table= True):
-    post:     int = Field(foreign_key="posts.post_id")
-    users:    str = Field(foreign_key="users.users_id")
+    
+class Post(Base):
+    __tablename__ = 'posts'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    content = Column(String, index=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    name = Column(String,ForeignKey('users.username'))
+    users = relationship('User', secondary=User_Post_association, back_populates='posts')
+    
+User.posts = relationship('Post', secondary=User_Post_association, back_populates='users')
